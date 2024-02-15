52e889017ca7def5b263c30d9c0c18f2ebfbb23e
Alessandro Ideo <alessandro.ideo@archinnova.org>
2024-02-15 14:29:55 -0500

    Added orm models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 # CarModelInline class
-
+class CarModelInline(admin.StackedInline):
+    model = CarMake 
 # CarModelAdmin class
 
 # CarMakeAdmin class with CarModelInline
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,8 +9,26 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    dealer_id = models.IntegerField(null=True)
+    name = models.CharField(max_length=200)
+    typ = models.CharField(max_length=200)
+    year = models.DateField(null=True)
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return "Dealer id: " + str(self.dealer_id) + "," + \
+            "Name: " + self.name + ", Type: " + self.typ + ", Year: " + str(self.year)
 
-
+class CarMake(models.Model):
+    name = models.CharField(null=True, max_length=100)
+    description = models.CharField(max_length=500)
+    model = models.ForeignKey(CarModel, null=True, on_delete=models.CASCADE)
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+            "Description: " + self.description
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
